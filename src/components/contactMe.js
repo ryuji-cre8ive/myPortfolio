189aa0b7a88b50d704e0fcbd1ab2699894e84ad6
ryuji-cre8ive <ryujisoccer0327@icloud.com>
2020-05-20 23:23:09 +0900

    addTwitterAndGithubIconToMyAccount

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -8,8 +8,8 @@ const contactMe = () => {
     <div className="contactMe">
       <p>Contact me</p>
       <div className="icons">
-        <a href="https://www.twitter.com" target="_brank" className="twitter"><FontAwesomeIcon icon={faTwitter} style={{width: 30, height: 30}} /></a>
-        <a href="https://www.github.com" target="_brank" className="github"><FontAwesomeIcon icon={faGithub} style={{width: 30, height: 30}}/></a>
+        <a href="https://twitter.com/ryuji_vlog" target="_brank" className="twitter"><FontAwesomeIcon icon={faTwitter} style={{width: 30, height: 30}} /></a>
+        <a href="https://github.com/ryuji-cre8ive" target="_brank" className="github"><FontAwesomeIcon icon={faGithub} style={{width: 30, height: 30}}/></a>
       </div>
     </div>
       
